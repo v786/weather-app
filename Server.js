04adcad9ff68a0449a24f2431b6c47619fbf989a
v786 <jahangirali786@gmail.com>
2018-06-05 16:45:29 +0530

    Finally done- Basic temperature in celsius

--- Server.js
+++ Server.js
@@ -1,33 +1,31 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express();
 const request = require('request');
+const app = express()
 
 const apiKey = '5dfb0a1289535cd275d6dfe6e6a6abc5';
 
-app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.set('view engine', 'ejs')
 
 app.get('/', function (req, res) {
-  res.render('index');
-});
+  res.render('index', {weather: null, error: null});
+})
 
 app.post('/', function (req, res) {
   let city = req.body.city;
-  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+
   request(url, function (err, response, body) {
     if(err){
       res.render('index', {weather: null, error: 'Error, please try again'});
-    } 
-    else {
+    } else {
       let weather = JSON.parse(body)
       if(weather.main == undefined){
         res.render('index', {weather: null, error: 'Error, please try again'});
-      } 
-      else {
-        let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+      } else {
+        let weatherText = `It's ${weather.main.temp} degree Celsius in ${weather.name}!`;
         res.render('index', {weather: weatherText, error: null});
       }
     }
@@ -35,5 +33,5 @@ app.post('/', function (req, res) {
 })
 
 app.listen(3000, function () {
-  console.log('Weather app listening on port 3000!')
-});
+  console.log('WeatherApp app listening on port 3000!')
+})
